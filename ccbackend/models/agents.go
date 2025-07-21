645d45ae2c3d2bc1beadeb25753b527c4a93dc83
preslavmihaylov <preslav@pmihaylov.com>
2025-07-21 12:04:00 +0300

    break ccbackend

--- ccbackend/models/agents.go
+++ ccbackend/models/agents.go
@@ -6,10 +6,11 @@ import (
 	"github.com/google/uuid"
 )
 
-type ActiveAgent struct {
-	ID               uuid.UUID  `json:"id" db:"id"`
-	AssignedJobID    *uuid.UUID `json:"assigned_job_id" db:"assigned_job_id"`
-	WSConnectionID   string     `json:"ws_connection_id" db:"ws_connection_id"`
-	CreatedAt        time.Time  `json:"created_at" db:"created_at"`
-	UpdatedAt        time.Time  `json:"updated_at" db:"updated_at"`
+type ActieAgent struct {
+	ID             uuid.UUID  `json:"id" db:"id"`
+	AssignedJobID  *uuid.UUID `json:"assigned_job_id" db:"assigned_job_id"`
+	WSConnectionID string     `json:"ws_connection_id" db:"ws_connection_id"`
+	CreatedAt      time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt      time.Time  `json:"updated_at" db:"updated_at"`
 }
+
