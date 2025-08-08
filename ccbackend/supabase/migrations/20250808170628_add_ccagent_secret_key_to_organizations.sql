0a31b7903d662158e58fc03c3195ead6106ad453
preslavmihaylov <preslav@pmihaylov.com>
2025-08-08 20:41:57 +0300

    remove broken migrations

--- ccbackend/supabase/migrations/20250808170628_add_ccagent_secret_key_to_organizations.sql
+++
@@ -1,9 +0,0 @@
--- Add ccagent_secret_key and ccagent_secret_key_generated_at to organizations table in production schema
-ALTER TABLE claudecontrol.organizations 
-ADD COLUMN ccagent_secret_key VARCHAR(512) NULL,
-ADD COLUMN ccagent_secret_key_generated_at TIMESTAMPTZ NULL;
-
--- Add ccagent_secret_key and ccagent_secret_key_generated_at to organizations table in test schema
-ALTER TABLE claudecontrol_test.organizations 
-ADD COLUMN ccagent_secret_key VARCHAR(512) NULL,
-ADD COLUMN ccagent_secret_key_generated_at TIMESTAMPTZ NULL;
